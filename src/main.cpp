e69e1b0597d068568995850b9814984e53e8b204
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-05-20 08:54:46 +0200

    ssid and password check

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <SPIFFS.h>
 
 // Default network credentials
-char ssid[32] = "ESP32";
+char ssid[32] = "ESP32AP";
 char password[64] = "0123456789";
 
 // Create an instance of the AsyncWebServer
@@ -26,23 +26,29 @@ void setup()
 
   server.on("/save-credentials", HTTP_POST, [](AsyncWebServerRequest *request)
             {
-              String submittedSSID = request->arg("ssid");
-              String submittedPassword = request->arg("password");
+  String submittedSSID = request->arg("ssid");
+  String submittedPassword = request->arg("password");
 
+  // Check if the submitted SSID is not empty and the password has at least 8 characters
+  if (submittedSSID.length() > 0 && submittedPassword.length() >= 8) {
+    // Update the credentials
+    strncpy(ssid, submittedSSID.c_str(), sizeof(ssid));
+    strncpy(password, submittedPassword.c_str(), sizeof(password));
 
-              // Update the credentials
-              strncpy(ssid, submittedSSID.c_str(), sizeof(ssid));
-              strncpy(password, submittedPassword.c_str(), sizeof(password));
+    // Print the updated credentials
+    Serial.println("Updated SSID: " + String(ssid));
+    Serial.println("Updated Password: " + String(password));
+    
+    request->send(200, "text/plain", "Credentials updated");
+    // Disconnect any connected clients
+    WiFi.softAPdisconnect(true);
+    // Configure ESP32 as an access point with new credentials
+    WiFi.softAP(ssid, password);
 
-              // Print the updated credentials
-              Serial.println("Updated SSID: " + String(ssid));
-              Serial.println("Updated Password: " + String(password));
-
-              request->send(200, "text/plain", "Credentials updated");
-              // Disconnect any connected clients
-              WiFi.softAPdisconnect(true);
-              // Configure ESP32 as an access point with new credentials
-              WiFi.softAP(ssid, password); });
+    
+  } else {
+    request->send(400, "text/plain", "Invalid credentials. SSID must not be empty and password must have at least 8 characters.");
+  } });
 
   // Start the server
   server.begin();
