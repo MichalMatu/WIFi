45d9723a81d6548fb0933d1d3ae2f51d8a3c0933
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-05-20 08:25:11 +0200

    credentials fixed

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,16 @@
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 
-// Replace with your network credentials
-const char *ssid = "ESP32-Access-Point";
-const char *password = "0123456789";
+// Default network credentials
+char ssid[32] = "ESP32-Access-Point";
+char password[64] = "0123456789";
 
 // Create an instance of the AsyncWebServer
 AsyncWebServer server(80);
 
 void setup()
 {
+  Serial.begin(115200);
   // Configure ESP32 as an access point
   WiFi.softAP(ssid, password);
 
@@ -23,10 +24,31 @@ void setup()
   // Serve static files from SPIFFS
   server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
 
+  server.on("/save-credentials", HTTP_POST, [](AsyncWebServerRequest *request)
+            {
+              String submittedSSID = request->arg("ssid");
+              String submittedPassword = request->arg("password");
+
+              // Update the credentials
+              strncpy(ssid, submittedSSID.c_str(), sizeof(ssid));
+              strncpy(password, submittedPassword.c_str(), sizeof(password));
+
+              // Print the updated credentials
+              Serial.println("Updated SSID: " + String(ssid));
+              Serial.println("Updated Password: " + String(password));
+
+              request->send(200, "text/plain", "Credentials updated");
+              // Disconnect any connected clients
+              WiFi.softAPdisconnect(true);
+              // Configure ESP32 as an access point with new credentials
+              WiFi.softAP(ssid, password);
+            });
+
   // Start the server
   server.begin();
 }
 
 void loop()
 {
+  // Your loop code here
 }
